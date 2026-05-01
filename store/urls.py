1e43f1d0f13e23fec090360653ab5113272b8e16
unknown <mathusush1106@gmail.com>
2026-05-01 14:37:24 +0530

    updated homepage design and other page backgrounds with order button enabled

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from .views import home
+from . import views
 
 urlpatterns = [
     path('', home, name='home'),
+    path('sarees/', views.sarees, name='sarees'),
+    path('dryfruits/', views.dryfruits, name='dryfruits'),
+    path('healthmixes/', views.healthmixes, name='healthmixes'),
+    path('contact/', views.contact, name='contact'),
+    path('reviews/', views.reviews, name='reviews'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,11 +3,28 @@ from .models import Product
 from django.utils.timezone import now
 
 def home(request):
+    products = Product.objects.filter(expiry_date__gte=now().date())
+    return render(request, 'home.html', {'products': products})
 
-    products = Product.objects.filter(
-        expiry_date__gte=now().date()
-    )
 
-    return render(request, 'home.html', {
-        'products': products
-    })
+def sarees(request):
+    products = Product.objects.filter(category='saree')
+    return render(request, 'sarees.html', {'products': products})
+
+
+def dryfruits(request):
+    products = Product.objects.filter(category='dryfruit')
+    return render(request, 'dryfruits.html', {'products': products})
+
+
+def healthmixes(request):
+    products = Product.objects.filter(category='healthmix')
+    return render(request, 'healthmixes.html', {'products': products})
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def reviews(request):
+    return render(request, 'reviews.html')
